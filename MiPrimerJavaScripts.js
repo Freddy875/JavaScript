bc2b2dbd6544e2cf0c4b7c65e1046fa756f824ea
Freddy875 <fer875nandosm@gmail.com>
2020-07-11 20:35:11 -0500

    Estamos aprendiendo JavaScript

---
+++ MiPrimerJavaScripts.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h2>Mi Primer JavaScript</h2>
+
+    <button type="button" 
+    onclick="document.getElementById('demo'.innerHTML) = Date()">
+        Haz Click en mi para mostrar la fecha y la hora.
+    </button>
+
+    <p id="demo"></p>
+
+</body>
+</html>
